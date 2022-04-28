ba08136fd0cb69e57302c93264313b09b88ad8cc
salim1832 <salimfawwazmuwaffaq@gmail.com>
2022-04-28 14:06:40 +0700

    praktikum9

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { Route, Routes } from "react-router-dom";
+import CreateMovie from "./pages/movie/Create";
+import Home from "./pages/Home";
+import PopularMovie from "./pages/movie/Popular";
+import NowPlayingMovie from "./pages/movie/NowPlaying";
+import TopRatedMovie from "./pages/movie/TopRated";
+import Layout from "./layout";
+
+function App(){
+    return (
+        <>
+        <Layout>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/movie/create" element={<CreateMovie />} />
+                <Route path="/movie/popular" element={<PopularMovie />} />
+                <Route path="/movie/now" element={<NowPlayingMovie/>} />
+                <Route path="/movie/top" element={<TopRatedMovie/>} />
+            </Routes>
+        </Layout>
+        </>
+    );
+}
+
+export default App;
